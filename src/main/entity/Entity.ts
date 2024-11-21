f5c7bacc90530d84766d524ea9875464893cb935
董野 <30040852@qq.com>
2024-11-21 17:19:25 +0800

    定义配置类型

--- src/main/entity/Entity.ts
+++ src/main/entity/Entity.ts
@@ -24,10 +24,12 @@ export type unzipEntity = {
     process: number
 }
 
-
 export type serverEntity = {
     name: string
+    cover: string
     map: string
+    mod_map: boolean
+    use_api: boolean
     port: number
     query_port: number
     rconPort: number
@@ -35,17 +37,44 @@ export type serverEntity = {
     observe_password: string
     cluster_id: string
     cluster_path: string
-    mods: string[]
-    user_config:serverUserConfigEntity
-    game_config:serverGameConfigEntity
-    state:string
-    state_text:string
+    language: string
+    mod_ids: number[]
+    mod_names: string[]
+    mods: serverModEntity[]
+    args: string[]
+    user_config: serverUserConfigEntity
+    game_config: serverGameConfigEntity
+    state: string
+    state_text: string
+    server_download: boolean
+    runtime_state: serverRuntimeStateEntity
+    back_up: serverBackupEntity
 }
 
-export type serverGameConfigEntity = {
-
+export type serverRuntimeStateEntity = {
+    state: string
+    state_text: string
+    max_player_count: number
+    player_cont: number
+    days: number
+    players: []
+    tribes: []
 }
 
-export type serverUserConfigEntity = {
-
+export type serverBackupEntity = {
+    enable: boolean
+    type: 'fixed' | 'interval'
+    time_hour: number
+    time_minute: number
+    max_reserve_day: number
+    backup_path: string
+    backup_world_path: string
+    backup_tribe_path: string
+    backup_player_path: string
 }
+
+export type serverModEntity = {}
+
+export type serverGameConfigEntity = {}
+
+export type serverUserConfigEntity = {}
